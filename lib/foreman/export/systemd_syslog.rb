646d78e868ed03e1fa7b55f1fa0a290666a8d16f
Zach Davis <zldavis@gmail.com>
2016-11-30 17:33:17 -0800

    specify template root

--- lib/foreman/export/systemd_syslog.rb
+++ lib/foreman/export/systemd_syslog.rb
@@ -21,7 +21,7 @@ class Foreman::Export::SystemdSyslog < Foreman::Export::Base
 
     engine.each_process do |name, process|
       service_fn = "#{app}-#{name}@.service"
-      write_template "systemd-syslog/process.service.erb", service_fn, binding
+      write_template "process.service.erb", service_fn, binding
 
       create_directory("#{app}-#{name}.target.wants")
       1.upto(engine.formation[name])
@@ -31,10 +31,15 @@ class Foreman::Export::SystemdSyslog < Foreman::Export::Base
         create_symlink("#{app}-#{name}.target.wants/#{process_name}", "../#{service_fn}") rescue Errno::EEXIST # This is needed because rr-mocks do not call the origial cleanup
       end
 
-      write_template "systemd-syslog/process_master.target.erb", "#{app}-#{name}.target", binding
+      write_template "process_master.target.erb", "#{app}-#{name}.target", binding
       process_master_names << "#{app}-#{name}.target"
     end
 
-    write_template "systemd-syslog/master.target.erb", "#{app}.target", binding
+    write_template "master.target.erb", "#{app}.target", binding
+  end
+
+  def write_template(name, target, binding)
+    compiled = ERB.new(export_template("systemd-syslog", name, "systemd-syslog"), nil, '-').result(binding)
+    write_file target, compiled
   end
 end
